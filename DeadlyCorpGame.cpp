1e1ff2dec381285f5e05ff218c74e13108f3fdaf
Khai Do <doyky006@mymail.unisa.edu.au>
2024-03-16 09:09:52 +1030

    Add project files.

---
+++ DeadlyCorpGame.cpp
@@ -0,0 +1,12 @@
+﻿// DeadlyCorpGame.cpp : Defines the entry point for the application.
+//
+
+#include "DeadlyCorpGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, Im going to develop Deadly Corp Game" << endl;
+	return 0;
+}
